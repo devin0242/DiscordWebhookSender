cca943a1ad1881a975c74f482a013e1d6a798a14
djspicer05 <36250529+djspicer05@users.noreply.github.com>
2021-07-08 01:12:09 -0400

    Update WebhookSender2.py
    made the UI more organized, and looking better than before.

--- WebhookSender2.py
+++ WebhookSender2.py
@@ -5,22 +5,26 @@ import requests
 from io import BytesIO
 from tkinter import filedialog
 import time
+import discord
 root=tk.Tk()
 root.title("Discord Webhook Sender")
 root.config(width=1000, height=500)
 
 root.resizable(False,False)
 
+
+
+
 def error_popup_window(error=None):
     window = tk.Toplevel()
 
     label = tk.Label(window, text=f"There was an error!: {error}")
     label.grid(row=0, column=0)
-    time.sleep(5)
-    window.destroy()
+
 def previewImage():
     global img
     try:
+        
         img_url = pfpEntry.get()
         response = requests.get(img_url)
         img_data = response.content
@@ -33,16 +37,18 @@ def previewImage():
     except Exception as E:
         error_popup_window(E)
 
+
 #title
 title=tk.Label(text="Send Discord Webhooks")
 title.config(font=("helvetica",15))
 title.grid(row=0, column=0)
 
+
 #webhook name stuff
 #webhook name LABEL
 name_column_default=0
-input_name_label=tk.Label(text="Input the name of the webhook you want to send.             ")
-input_name_label.grid(row=1, column=name_column_default)
+input_name_label=tk.Label(text="Input the name of the webhook you want to send.")
+input_name_label.grid(row=1, column=name_column_default,padx=25)
 #webhook name input
 hookNAME_entry=tk.Entry(root)
 hookNAME_entry.grid(row=2,column=name_column_default)
@@ -50,11 +56,11 @@ hookNAME_entry.grid(row=2,column=name_column_default)
 #webhook content stuff
 #webhook content LABEL
 content_column_default=name_column_default+1
-input_hookcontent_label=tk.Label(text="Input the message content of the webhook you want to send.             ")
-input_hookcontent_label.grid(row=1, column=content_column_default)
+input_hookcontent_label=tk.Label(text="Input the message content of the webhook you want to send.")
+input_hookcontent_label.grid(row=1, column=content_column_default,padx=25)
 #content input box
-content_entry=tk.Entry(root)
-content_entry.grid(row=2,column=content_column_default)
+content_entry=tk.Text(root, height=5, width=50)
+content_entry.grid(row=2,column=content_column_default,rowspan=2)
 
 #webhook link stuff
 #webhook link LABEL
@@ -63,27 +69,27 @@ input_hookURL_label=tk.Label(text="Input the WebHook URL")
 input_hookURL_label.grid(row=1, column=link_column_default)
 
 #webhook input box
-hookUrl_entry=tk.Entry(root)
-hookUrl_entry.grid(row=2,column=link_column_default)
+hookUrl_entry=tk.Entry(root,width=60)
+hookUrl_entry.grid(row=2,column=link_column_default,padx=25)
 
 #profile picture stuff
 
 #image url input box LABEL
 img_column_default=link_column_default+1
 input_img_url_label=tk.Label(text="Input the Image you would like to use as the webhook's profile picture.")
-input_img_url_label.grid(row=1, column=img_column_default)
+input_img_url_label.grid(row=1, column=img_column_default,padx=25)
 #image url input box
 pfpEntry=tk.Entry(root)
 pfpEntry.grid(row=2,column=img_column_default)
 #preview image button
-img_url_confirm_button=tk.Button(text="preview your image",command=previewImage)
-img_url_confirm_button.grid(row=3, column=img_column_default)
+img_url_confirm_button=tk.Button(text="Preview your image",command=previewImage)
+img_url_confirm_button.grid(row=3, column=img_column_default,pady=9)
 
 #sending the webhook
 def sendWebhook ():
     try:
         url = hookUrl_entry.get()
-        content=content_entry.get()
+        content=content_entry.get('1.0', 'end-1c')
         name=hookNAME_entry.get()
         avatar_url=pfpEntry.get()
         data = {
@@ -93,22 +99,25 @@ def sendWebhook ():
         }
 
         result = requests.post(url, json = data)
+        
         window = tk.Toplevel()
 
         label = tk.Label(window, text=f"WebHook Sent Successfully!!")
         label.grid(row=0, column=0)
     except Exception as E:
         error_popup_window(E)
-#send webhook LABEL
-send_webhook_column_default=img_column_default+2
-sendWebhookLabel=tk.Label(text="Send the Webhook!").grid(row=1, column=send_webhook_column_default)
-
+#send webhook 
+send_webhook_column_default=img_column_default-1
 
 #send webhook button
-sendWebhook_button=tk.Button(text="Send the Webhook",command=sendWebhook)
-sendWebhook_button.grid(row=3, column=send_webhook_column_default)
+sendWebhook_button=tk.Button(text="Send the Webhook",command=sendWebhook,bg='pale green',height=4,width=40, activebackground="forest green")
+sendWebhook_button.grid(row=4, column=send_webhook_column_default,pady=10,rowspan=2,columnspan=3)
+
 
 import json
+
+
+
 
 s2jLABEL=tk.Label(text="Save the settings in the boxes above to a json file.").grid(row=4, column=0)
 def sendWebhookFromJSONdata(content,name, avatar_url,url):
@@ -119,8 +128,8 @@ def sendWebhookFromJSONdata(content,name, avatar_url,url):
         }
         
         result = requests.post(url, json = data)
-        print(data,url)
-        print(result)
+        #print(data,url)
+        #print(result)
 
 
     except Exception as E:
@@ -142,10 +151,10 @@ def showSettingsFromJSON():
         divider=tk.Label(root,text="----------------------------------------").grid(row=1, column=0)
     
         msg_content_box_label=tk.Label(root,text="Input the message content you want to send in the webhook.").grid(row=2, column=0)
-        msg_content_box=tk.Entry(root)
+        msg_content_box=tk.Text(root,height=5, width=100)
         msg_content_box.grid(row=3, column=0)
-        webhookSendBTN=tk.Button(root,text=f"Send Webhook with data from {filename}", command=lambda: sendWebhookFromJSONdata(msg_content_box.get(),name,avatar_url,url))
-        webhookSendBTN.grid(row=4, column=0)
+        webhookSendBTN=tk.Button(root,text=f"Send Webhook with data from {filename}",bg='pale green',height=4,padx=10, activebackground="forest green", command=lambda: sendWebhookFromJSONdata(msg_content_box.get('1.0', 'end-1c'),name,avatar_url,url))
+        webhookSendBTN.grid(row=4, column=0,pady=9)
     except Exception as E:
         error_popup_window(E)
 def file_save():
@@ -162,9 +171,10 @@ def file_save():
     }
     with open(file, "w") as f:    
         json.dump(data,f)
-save2json=tk.Button(text="Save Settings as JSON",command=file_save).grid(row=6, column=0)
-lfJSONlabel=tk.Label(text="Load data from JSON file.").grid(row=5, column=1)
+save2json=tk.Button(text="Save Settings as JSON",command=file_save).grid(row=5, column=0)
+lfJSONlabel=tk.Label(text="Load data from JSON file.").grid(row=4, column=1)
 loadfromjson=tk.Button(text="load from json",command=showSettingsFromJSON)
-loadfromjson.grid(row=6, column=1)
+loadfromjson.grid(row=5, column=1)
+
 
 root.mainloop()
